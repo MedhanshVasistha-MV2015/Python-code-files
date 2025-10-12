eb95f99e11b6b4e488ef253136c4e0bdb477abb7
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-12 10:51:53 +0530

    Add files via upload

---
+++ L3A2.py
@@ -0,0 +1,8 @@
+a = 10
+print ('data type of variable a is: ', type (a))
+s =float(a)
+print ('data type of variable s after typecasting is: ', type (s))
+b = 10.3
+print ('data type of variabe b is: ' , type (b))
+t = int(b)
+print ('data type of variable t after typecasting is: ', type (t))
