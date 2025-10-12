955b961110b6722ff6b6d3e36778a9b35d0d8ece
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-12 10:54:00 +0530

    proL2_2project.py

--- Lesson 2/L2_2project.py
+++ Lesson 2/L2_2project.py
@@ -1,3 +1,4 @@
-Birthday = input(3/12/2015)
-print('My creators birthday is on', Birthday , end='!')
-print('when is your birthday', end='?' )
+b = input('type your birthday here: ')
+n = input('type your name here: ')
+print(b)
+print(n)
