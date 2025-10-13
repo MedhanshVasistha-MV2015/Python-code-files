bf83db9417a2d2511fd559e3c11fe8eaf26ee200
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-13 17:02:46 +0530

    Add files via upload

---
+++ Lesson 6/L6A1.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12
+c = -10
+if a and b and c :
+    print ('All the numbers have the boolean value as true.')
+else:
+    print ('Some of the numbers have the the boolean value as false.')
+
+
+d = 14
+e = 16
+f = -10
+if d > 0 or f > 0:
+    print ('Either of the number is greater than 0.')
+else:
+    print ('No number is greater than 0.')
