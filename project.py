4172f909f542b9e3c35a8094cd8fb45c106bd75b
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-17 11:06:49 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+"""Raj wants to know how big his basketball and football is. basketball:5 inches football:3 inches"""
+ball1 = 5
+ball2 = 3
+total = 8
+print('The total of the two balls are: ', total) 
+average = total/5
+print ('the average height of the ball is: ', average)
