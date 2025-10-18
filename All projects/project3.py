3da292e9df1239720c9e0aff6bd76f7a820c9a85
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-18 10:59:31 +0530

    Add files via upload

---
+++ All projects/project3.py
@@ -0,0 +1,6 @@
+var = input('type the character: ')
+var2 = type(var)
+if (('A' <= var and var <= 'Z') or ('a'<= var and var <= 'z')):
+    print('It is an alphabet.')
+else:
+    print('It is not an alphabet.')
