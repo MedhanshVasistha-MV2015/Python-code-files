9baed941849d5967b585c7b04c84a69e6925fd51
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-10-28 17:59:31 +0530

    Add files via upload

---
+++ Lesson 8/L8.py
@@ -0,0 +1,15 @@
+a = 5
+b = 3
+c = 9
+d = 7
+e = 0
+e = (a+b)*c+d
+print('The value of (a+b)*c+d is ', e)
+
+
+name = 'Sam'
+age = 0
+if name == "Sam" or name == 'Jeff' and age >= 2:
+    print('True')
+else:
+    print('False')
