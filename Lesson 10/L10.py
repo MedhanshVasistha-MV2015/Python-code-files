9b619ad1169043958e4e988598682f2c123acb5d
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-11-02 11:45:43 +0530

    Add files via upload

---
+++ Lesson 10/L10.py
@@ -0,0 +1,6 @@
+n = int(input ('type the number you want to calculate: '))
+sum = 0
+for i in range (1 , n+1):
+    print('the sum of ' , i , ' and ' , sum , ' is ')
+    sum = sum + i
+    print(sum)
