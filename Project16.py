af7a3c399aed3a052a7e5b820be6b4d5d854cfda
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2025-12-28 08:38:18 +0530

    Add files via upload

---
+++ Project16.py
@@ -0,0 +1,9 @@
+year = int(input("Enter a year: "))
+age = 2025 - year
+
+print('\nYou are ' , age , ' years old in 2025.')
+if age % 2 == 0:
+    print('The age ' , age , ' is even.')
+else:
+    print('The age ' , age , ' is odd.')
+
