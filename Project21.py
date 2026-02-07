e8b56e105e49d55fca08ff09d9c115562e07def4
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2026-02-07 10:58:29 +0530

    Add files via upload

---
+++ Project21.py
@@ -0,0 +1,12 @@
+Set1 = {'blue', 'green'} 
+Set2 = {'blue', 'yellow'}
+symmetric_differencecolor = Set1.symmetric_difference(Set2)
+print('Current sets: ', Set1, Set2, '\n symmetric difference of the sets: ', symmetric_differencecolor)
+
+
+
+
+Set3 = {1, 2, 3, 4, 5} 
+Set4 = {1, 5, 6, 7, 8, 9}
+symmetric_differencenum = Set3.symmetric_difference(Set4)
+print('Current sets: ', Set3, Set4, '\n symmetric difference of the sets: ', symmetric_differencenum)
