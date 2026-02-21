92e858a71c913837207a3bf0080a2a8ff21c7b72
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2026-02-21 10:57:58 +0530

    Add files via upload

---
+++ Lesson 29/L29_3.py
@@ -0,0 +1,31 @@
+'''Write a program to create a base class Bird, with a constructor and two
+methods. Then, create a child class that inherits the constructor from Class Bird
+and has two functions. Finally, display how you can use Super to access the
+parent class constructor inside the child class.Write a program to create a base class Bird, with a constructor and two
+methods. Then, create a child class that inherits the constructor from Class Bird
+and has two functions. Finally, display how you can use Super to access the
+parent class constructor inside the child class.'''
+class Bird:
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+
+    def swim(self):
+        print("Swim faster")
+class Penguin(Bird):
+
+    def __init__(self):
+         super().__init__()
+    print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+peggy = Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
