d9259de3753ab92357ceebf8b143b1a07e1ec196
MedhanshVasistha-MV2015 <vasistha.medhansh@gmail.com>
2026-02-22 10:43:07 +0530

    Add files via upload

---
+++ Lesson 31/L31_2.py
@@ -0,0 +1,27 @@
+'''Write a program to implement abstraction on animal class (base class). The
+abstract method will be move will display what subclasses can do. Subclasses
+can be something like - Human, Dog.'''
+from abc import ABC, abstractmethod
+class Animal(ABC):
+	def move(self):
+		pass
+class Human(Animal):
+	def move(self):
+		print("I can walk and run")
+class Snake(Animal):
+	def move(self):
+		print("I can crawl")
+class Dog(Animal):
+	def move(self):
+		print("I can bark")
+class Lion(Animal):
+	def move(self):
+		print("I can roar")
+R = Human()
+R.move()
+K = Snake()
+K.move()
+R = Dog()
+R.move()
+K = Lion()
+K.move()
